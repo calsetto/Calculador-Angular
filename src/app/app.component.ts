d9da70374e21ba81a1ebab2ed305592f8835a84f
calsetto <importante537@gmail.com>
2020-03-14 16:09:34 -0400

    VERSION 2 (CALCULADORA FUNCIONAL)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Calculadora';
+
+  result: string = '';
+  private preValue: string = '';
+  private curValue: string = '';
+   
+  add(value:string)
+  {
+    if(this.result != '')
+    {
+      this.preValue = this.curValue;
+      this.curValue = value;
+    }
+    if(value == 'AC')
+    {
+      this.result = '';
+    }
+    else if(value == '=')
+    {
+      this.result = eval(this.result);
+    }
+    else
+    {
+      this.result += value;
+    }
+  }
+
 }
